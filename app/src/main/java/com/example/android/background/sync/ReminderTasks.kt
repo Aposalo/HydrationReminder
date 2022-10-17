afcf8f22403fe5916ec9c12c4c8fd00f90c784f2
aposalo <apostolissalomidis@yahoo.gr>
2022-10-17 21:59:53 +0300

    Code cleanup

--- app/src/main/java/com/example/android/background/sync/ReminderTasks.kt
+++ app/src/main/java/com/example/android/background/sync/ReminderTasks.kt
@@ -3,16 +3,16 @@ package com.example.android.background.sync
 import android.content.Context
 import com.example.android.background.utilities.PreferenceUtilities
 
-public class ReminderTasks {
+class ReminderTasks {
 
     companion object {
-        public const val ACTION_INCREMENT_WATER_COUNT:String = "increment-water-count";
+        const val ACTION_INCREMENT_WATER_COUNT:String = "increment-water-count"
 
-        public fun incrementWaterCount(context: Context) {
+        private fun incrementWaterCount(context: Context) {
             PreferenceUtilities.incrementWaterCount(context)
         }
 
-        public fun executeTask(context: Context, action: String) {
+        fun executeTask(context: Context, action: String) {
             if (action == ACTION_INCREMENT_WATER_COUNT)
                 incrementWaterCount(context)
         }
--- app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.kt
+++ app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.kt
@@ -3,15 +3,7 @@ package com.example.android.background.sync
 import android.app.IntentService
 import android.content.Intent
 
-/*https://developer.android.com/reference/kotlin/android/app/IntentService*/
-
-/*
-IntentService is subject to all the background execution limits imposed with Android 8.0 (API level 26).
-Consider using androidx.work.WorkManager or androidx.core.app.JobIntentService, which uses jobs instead
-of services when running on Android 8.0 or higher.
-*/
-
-public class WaterReminderIntentService() : IntentService("WaterReminderIntentService") {
+class WaterReminderIntentService : IntentService("WaterReminderIntentService") {
 
     override fun onHandleIntent(intent: Intent?) {
         val action = intent!!.action
